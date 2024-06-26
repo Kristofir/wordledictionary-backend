8522c6ce19c44df13459f0be24435612b1b450ce
Chris Wan <hello@chriswan.me>
2024-06-25 18:16:22 -0700

    Added dev mode

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+import { parseArgs } from "util";
+
 import { 
   search
 } from "run/search/search";
@@ -10,7 +12,27 @@ console.log("Server started");
 
 defineArrayIntersectOverride()
 
+
+const { values } = parseArgs({
+  args: Bun.argv,
+  options: {
+    watch: {
+      type: 'boolean',
+    },
+    dev: {
+      type: 'boolean',
+    },
+  },
+  strict: false,
+  allowPositionals: true,
+});
+
+if (values.dev) {
+  process.env.NODE_ENV = 'development'
+}
+
 Bun.serve({
+  development: values.dev ? true : false,
   async fetch(req: Request) {
     console.log(req)
     const url = new URL(req.url);
@@ -19,8 +41,25 @@ Bun.serve({
     if (url.pathname === "/search") {
       console.log("Search params: ", searchParams)
       const searchResults = await search(searchParams)
+
+      const compressedSearchResults = Bun.gzipSync(JSON.stringify(searchResults))
+
+      const response = new Response(compressedSearchResults);
+
+      response.headers.set('Content-Type', 'text/html');
+      response.headers.set('Content-Encoding', 'gzip');
+
+      console.log(process)
+
+      if (process.env.NODE_ENV === 'development') {
+        response.headers.set('Access-Control-Allow-Origin', '*');
+        response.headers.set('Cross-Origin-Resource-Policy', 'cross-origin');
+        response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+      }
+
+      console.log(response.headers)
       
-      return new Response(JSON.stringify(searchResults))
+      return response
     };
 
     return new Response("Success!");
